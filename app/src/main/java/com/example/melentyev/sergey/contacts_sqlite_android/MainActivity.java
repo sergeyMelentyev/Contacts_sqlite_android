5fffa2e45c997c95a84de4d55087f527f9437213
sergeyMelentyev <sergey.melentyev@gmail.com>
2017-02-14 12:19:10 +0300

    new version

--- app/src/main/java/com/example/melentyev/sergey/contacts_sqlite_android/MainActivity.java
+++ app/src/main/java/com/example/melentyev/sergey/contacts_sqlite_android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.melentyev.sergey.contacts_sqlite_android;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("data.db", MODE_PRIVATE, null);
+        db.execSQL("DROP TABLE IF EXISTS contacts;");
+        db.execSQL("CREATE TABLE contacts(name TEXT, phone INTEGER, email TEXT);");
+        db.execSQL("INSERT INTO contacts VALUES('Sergey', 8985, 'seg@mail.ru');");
+        db.execSQL("INSERT INTO contacts VALUES('Olga', 8910, 'olga@mail.ru');");
+
+        Cursor query = db.rawQuery("SELECT * FROM contacts;", null);
+        if (query.moveToFirst()) {
+            do {
+                String name = query.getString(0);
+                int phone = query.getInt(1);
+                String email = query.getString(2);
+                Toast.makeText(this, name + " " + phone + " " + email, Toast.LENGTH_LONG).show();
+            } while (query.moveToNext());
+        }
+
+        query.close();
+        db.close();
     }
 }
